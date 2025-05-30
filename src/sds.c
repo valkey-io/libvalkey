f6e9022581d42403da8074cbdafedae52a4b7d40
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-05-30 14:21:59 -0400

    spelling: terminated
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/sds.c
+++ src/sds.c
@@ -707,7 +707,7 @@ fmt_error:
 }
 
 /* Remove the part of the string from left and from right composed just of
- * contiguous characters found in 'cset', that is a null terminted C string.
+ * contiguous characters found in 'cset', that is a null terminated C string.
  *
  * After the call, the modified sds string is no longer valid and all the
  * references must be substituted with the new pointer returned by the call.
