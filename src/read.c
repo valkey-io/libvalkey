af92d1cd11e5298a009b6aa30f8313d94b6359e5
michael-grunder <michael.grunder@gmail.com>
2026-03-10 08:18:39 -0700

    Additional overflow protection for MAP/ATTR
    There is already overflow protection for multibulk `elements` but the
    new RESP3 `MAP` and `ATTR` types subsequently double the count.
    
    This meant that a malicious or erroneous count of `LLONG_MAX / 2 + 1`
    would end up overflowing when maxelements = 0.
    
    Signed-off-by: michael-grunder <michael.grunder@gmail.com>

--- src/read.c
+++ src/read.c
@@ -556,8 +556,23 @@ static int processAggregateItem(valkeyReader *r) {
 
             moveToNextTask(r);
         } else {
-            if (cur->type == VALKEY_REPLY_MAP || cur->type == VALKEY_REPLY_ATTR)
+            if (cur->type == VALKEY_REPLY_MAP ||
+                cur->type == VALKEY_REPLY_ATTR) {
+
+                long long maxelements = LLONG_MAX / 2;
+                if (LLONG_MAX > SIZE_MAX &&
+                    maxelements > (long long)(SIZE_MAX / 2)) {
+                    maxelements = (long long)(SIZE_MAX / 2);
+                }
+
+                if (elements > maxelements) {
+                    valkeyReaderSetError(r, VALKEY_ERR_PROTOCOL,
+                                         "Multi-bulk length out of range");
+                    return VALKEY_ERR;
+                }
+
                 elements *= 2;
+            }
 
             if (r->fn && r->fn->createArray)
                 obj = r->fn->createArray(cur, elements);
